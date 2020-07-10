432745532ffaa649c3a9927dd1d31cef888d0cff
tuanle0205 <67400655+tuanle0205@users.noreply.github.com>
2020-07-10 08:32:25 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox2 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox3 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox4 = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBox5 = new System.Windows.Forms.ToolStripComboBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(251, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Topic";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(332, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "SubTopic";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(413, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Relationship";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(494, 43);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Summary";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(575, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Boundary";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(656, 43);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Note";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Crimson;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Location = new System.Drawing.Point(350, 160);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Central Topic";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(499, 80);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.Text = "Main Topic 1";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Click += new System.EventHandler(this.textBox2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(499, 160);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.Text = "Main Topic 2";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Click += new System.EventHandler(this.textBox3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(499, 241);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Main Topic 3";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.Click += new System.EventHandler(this.textBox4_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBox1,
+            this.toolStripComboBox2,
+            this.toolStripComboBox3,
+            this.toolStripComboBox4,
+            this.toolStripComboBox5});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1183, 25);
+            this.toolStrip1.TabIndex = 10;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(75, 25);
+            this.toolStripComboBox1.Text = "File";
+            // 
+            // toolStripComboBox2
+            // 
+            this.toolStripComboBox2.Name = "toolStripComboBox2";
+            this.toolStripComboBox2.Size = new System.Drawing.Size(75, 25);
+            this.toolStripComboBox2.Text = "Edit";
+            // 
+            // toolStripComboBox3
+            // 
+            this.toolStripComboBox3.Name = "toolStripComboBox3";
+            this.toolStripComboBox3.Size = new System.Drawing.Size(75, 25);
+            this.toolStripComboBox3.Text = "Insert";
+            // 
+            // toolStripComboBox4
+            // 
+            this.toolStripComboBox4.Name = "toolStripComboBox4";
+            this.toolStripComboBox4.Size = new System.Drawing.Size(75, 25);
+            this.toolStripComboBox4.Text = "View";
+            // 
+            // toolStripComboBox5
+            // 
+            this.toolStripComboBox5.Name = "toolStripComboBox5";
+            this.toolStripComboBox5.Size = new System.Drawing.Size(75, 25);
+            this.toolStripComboBox5.Text = "Help";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(13, 42);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = " Mind Map";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(104, 43);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Outliner";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(788, 42);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Zen";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(888, 42);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Share";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(1026, 42);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 15;
+            this.button11.Text = "Icon";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(1096, 42);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 16;
+            this.button12.Text = "Format";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(1183, 450);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox3;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox4;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WindowsFormsApp1
+{
+
+    public partial class Form1 : Form
+    {
+        
+        int cLeft = 4;
+        int dLeft = 1;
+        int bLeft = 4;
+        int Cen = 1;
+        int x = 3;
+        int y = 0;
+        int[] A = new int[10000];
+        Pen blackpen = new Pen(Color.Black, 2);
+        float x1, x2, y1, y2;
+        string a;
+        private PictureBox pictureBox1 = new PictureBox();
+
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 0; i < A.Length; i++)
+                A[i] = 1; 
+        }
+
+        private void Form1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+
+            e.Graphics.DrawLine(blackpen, x1, y1, x2, y2);
+            this.blackpen.Dispose();
+
+        }
+            private void button1_Click(object sender, EventArgs e)
+        {
+
+            AddNewTextBox();
+        }
+        public System.Windows.Forms.PictureBox AddNewTextBox()
+        {
+            x = x + 1;
+            x1 = 350;
+            y1 = 170;
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox(); //Khởi tạo đối tượng Textbox có tên là txt
+            this.Controls.Add(txt);  //Thêm đối tượng txt vào Form
+            txt.Top = cLeft * 25; //Thiết lập thuộc tính Top
+            txt.Left = 200; //Thiết lập thuộc tính Left
+            txt.Text = "Main Topic " + this.cLeft.ToString(); //Thiết lập thuộc tính Text cho Textbox
+            txt.Name = this.x.ToString();
+            x2 = 300;
+            y2 = cLeft * 25 - 10;
+            cLeft = cLeft + 1;
+            txt.Click += mybutton_click;
+            this.Controls.Add(pictureBox1);
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            return pictureBox1; 
+
+            
+        }
+        private void mybutton_click(object sender, EventArgs e)
+        {
+            Cen = 0;
+            TextBox txt1 = sender as TextBox;
+            a = txt1.Name;
+            
+        }
+        public System.Windows.Forms.TextBox AddNewTextBox1()
+        {
+            y = y + 1;
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox(); //Khởi tạo đối tượng Textbox có tên là txt
+            this.Controls.Add(txt);  //Thêm đối tượng txt vào Form
+            txt.Top = int.Parse(a) * 25; //Thiết lập thuộc tính Top
+            if (A[int.Parse(a)] == 1)
+            {
+                txt.Left = 85 ; //Thiết lập thuộc tính Left
+            }
+            else
+            {
+                txt.Left = 85 - (A[int.Parse(a)] - 1) * 110;
+            }
+            txt.Text = "Sub Topic " + this.A[int.Parse(a)].ToString(); //Thiết lập thuộc tính Text cho Textbox
+            txt.Name = this.y.ToString();
+            A[int.Parse(a)] = A[int.Parse(a)] + 1;
+            dLeft += 1;
+            txt.Click += mybutton_click;
+            return txt; //Trả lại đối tượng txt với các thuộc tính kèm theo
+        }
+
+        public System.Windows.Forms.TextBox AddNewTextBox2()
+        {
+            System.Windows.Forms.TextBox txt = new System.Windows.Forms.TextBox(); //Khởi tạo đối tượng Textbox có tên là txt
+            this.Controls.Add(txt);  //Thêm đối tượng txt vào Form
+            txt.Top = int.Parse(a) * 80; //Thiết lập thuộc tính Top
+            
+            if (A[int.Parse(a)] == 1)
+            {
+                txt.Left = 620; //Thiết lập thuộc tính Left
+            }
+            else
+            {
+                txt.Left = 620 + (A[int.Parse(a)]-1) * 110;
+            }
+            txt.Text = "Sub Topic " + this.A[int.Parse(a)].ToString(); //Thiết lập thuộc tính Text cho Textbox
+            A[int.Parse(a)] = A[int.Parse(a)] + 1;
+            dLeft = dLeft + 1;
+            bLeft = bLeft + 1;
+
+            return txt; //Trả lại đối tượng txt với các thuộc tính kèm theo
+        }
+
+
+
+            private void button2_Click(object sender, EventArgs e)
+            {
+                if (Cen == 1)
+                {
+                    AddNewTextBox();
+
+                }
+                else if (Cen == 2)
+                {
+                    AddNewTextBox2();
+                }
+                else { AddNewTextBox1(); };
+
+
+            }
+
+            private void textBox1_Click(object sender, EventArgs e)
+            {
+                Cen = 1;
+            }
+
+            private void textBox2_Click(object sender, EventArgs e)
+            {
+                Cen = 2;
+                a = "1";
+                
+            }
+
+            private void textBox3_Click(object sender, EventArgs e)
+            {
+                 Cen = 2;
+                 a = "2";
+                 
+            }
+
+        private void textBox4_Click(object sender, EventArgs e)
+        {
+            Cen = 2;
+            a = "3";
+            
+        }
+    }
+}
+
